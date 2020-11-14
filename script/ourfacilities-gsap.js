7a5dc8eeb9b25e4d55c8fc074f8aebaf71fad908
Nuh Ali <nuh_mali@hotmail.co.uk>
2020-11-14 18:11:51 +0000

    update gsap
    -Adjusted the initial position of the header elements
    -Adjusted the gsap animation of said elements from "from" to "to"
    -Increased the margin between the h1 and p, highlighted "insane"
    -created individual script folders for "ourfacilites" and "prices" pages

--- script/ourfacilities-gsap.js
+++ script/ourfacilities-gsap.js
@@ -1,3 +1,5 @@
+// Our facilities Page ------------------
+// Header
 window.addEventListener('load', () => {
     const timeline = gsap.timeline()
     timeline
@@ -21,83 +23,69 @@ window.addEventListener('load', () => {
             color: '#e65f21'
         })
 })
-
-// Features animation
-gsap.from('.feature', {
-    scrollTrigger: '.feature-trigger',
-    duration: 1,
-    opacity: 0,
-    y: '30px'
-})
-gsap.from('.learn-more', {
-    scrollTrigger: '.feature-trigger',
-    duration: 1,
-    opacity: 0,
-    y: '30px'
-})
-
-gsap.from('.image-container img', {
-    scrollTrigger: '.feature-trigger2',
-    delay: 0.5,
-    duration: 1,
-    opacity: 0,
-    x: '100vh',
-    ease: "power1",
-})
-
-// Gym shop animation
-gsap.from('.shop-description', {
-    scrollTrigger: '.feature-trigger2',
+// Gym description
+gsap.from('.gym-description', {
+    scrollTrigger: '.facilities-trigger',
     duration: 1,
     opacity: 0,
     y: '50px'
 })
 
-// Gym membership animation
-gsap.from('.membership', {
-    scrollTrigger: '.membership-trigger',
-    stagger: 0.3,
+gsap.from('.facility-features ul li', {
+    scrollTrigger: '.facilities-header',
+    stagger: 0.1,
+    delay: 1,
     duration: 0.7,
     opacity: 0,
     y: '50px'
 })
 
-// Join us animation
+// Image area
+gsap.from('.gallery-buttons', {
+    scrollTrigger: '.gallery-buttons',
+    duration: 1,
+    opacity: 0,
+    y: '50px'
+})
+
+gsap.from('.gallery img', {
+    scrollTrigger: '.gallery-buttons',
+    duration: 1,
+    opacity: 0,
+    y: '50px'
+})
+
+// Join us banner
+
 gsap.from('.banner-title', {
-    scrollTrigger: '.join-us',
     duration: 1,
     opacity: 0,
     y: '50px'
 })
 gsap.to(' .highlight-1', {
-    scrollTrigger: '.join-trigger',
     delay: 1.3,
     duration: 1,
     color: '#e65f21',
 })
 gsap.from('.join', {
-    scrollTrigger: '.join-us',
     duration: 1,
     opacity: 0,
 })
 
 // Contact us animation
 gsap.from('.contact-info h3', {
-    scrollTrigger: '.footer-trigger',
     duration: 1,
     opacity: 0,
     x: '-100vh'
 })
 
 gsap.from('.connect-us h3', {
-    scrollTrigger: '.footer-trigger',
     duration: 1,
     opacity: 0,
     x: '100vh'
 })
 
 gsap.from('.method', {
-    scrollTrigger: '.footer-trigger',
     stagger: 0.3,
     delay: 1,
     duration: 0.7,
@@ -106,7 +94,6 @@ gsap.from('.method', {
 })
 
 gsap.from('.fab', {
-    scrollTrigger: '.footer-trigger',
     stagger: 0.3,
     delay: 1,
     duration: 0.7,
@@ -117,18 +104,8 @@ gsap.from('.fab', {
 // Footer animation
 
 gsap.from('footer', {
-    scrollTrigger: '.footer-trigger',
     stagger: 0.3,
     delay: 2.3,
     duration: 0.7,
     opacity: 0,
-})
-
-// Sign up area
-window.addEventListener('load', () => {
-    gsap.from('form', {
-        duration: 1,
-        x: '-100vh',
-        opacity: 0
-    })
 })
---
+++ script/prices-gsap.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    const timeline = gsap.timeline()
+    timeline
+        .to('.main-title', {
+            duration: 1,
+            x: '0',
+            ease: "sine.in",
+        })
+        .to('.title-area h3', {
+            duration: 1,
+            x: '0',
+            ease: "sine.in",
+        })
+        .from('.title-button', {
+            duration: 1,
+            y: '50px',
+            opacity: 0
+        })
+        .to('.highlight', {
+            duration: 1,
+            color: '#e65f21'
+        })
+        .from('.membership', {
+            stagger: 0.3,
+            duration: 0.5,
+            opacity: 0,
+            y: '50px'
+        })
+})
+
+gsap.from('.join', {
+    scrollTrigger: '.membership',
+    duration: 1,
+    opacity: 0,
+})
